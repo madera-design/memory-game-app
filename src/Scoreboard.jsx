ac65b85b33f949def87d5f98c4f70f55855986fc
jose <joseantoniomaderag@gmail.com>
2025-02-01 12:26:18 -0600

    update UI component Scoreboard

--- src/Scoreboard.jsx
+++ src/Scoreboard.jsx
@@ -1,10 +1,10 @@
 const Scoreboard = ({ errors, matches, username}) => {
   return (
-    <div className="bg-white p-4 rounded-lg shadow-md mb-2 text-center">
+    <div className="bg-white p-4 rounded-lg shadow-md mb-2 flex text-center">
        <h2 className="text-md text-gray-600">Player: <span className="text-xl text-black font-bold mr-4">{username}</span></h2>
-      <div className="text-center flex space-x-4 text-lg">
-        <p className="text-md text-gray-600">Errors: <span className="text-2xl font-bold text-red-600">{errors}</span></p>
-        <p className="text-gray-600">Matches: <span className="text-2xl font-bold text-green-600">{matches}</span></p>
+      <div className="text-center flex space-x-4">
+        <h2 className="text-md text-gray-600">Errors: <span className="text-xl font-bold text-red-600">{errors}</span></h2>
+        <h2 className="text-md text-gray-600">Matches: <span className="text-xl font-bold text-green-600">{matches}</span></h2>
       </div>
     </div>
   );
